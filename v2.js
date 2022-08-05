c1def9f2f76e8d2a97b6ceb763f1b05a55d91c4a
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-06 01:01:04 +0900

    osaka

---
+++ v2.js
@@ -0,0 +1,225 @@
+const stores = {
+    'type': 'FeatureCollection',
+    'features': [{
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.49882344601633, 34.70251955199873]
+            },
+            'properties': {
+                'title': '阪急百貨店梅田店コンコース',
+                'address': '〒530-8350 大阪府大阪市北区角田町８−７',
+                'date': 'ここにいるのにいないような気持ちになれる落ち着く場所。',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/7780c520f651106ee75ab6160d9b6e87.png',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/1206/p-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.47569470365949, 34.631793416745694]
+            },
+            'properties': {
+                'title': '千本松渡船場',
+                'address': '大阪府大阪市大正区南恩加島 西成区南津守',
+                'date': '千本松渡船場は、大阪市の運営する8か所の渡船場のうち、利用者が比較的多い渡船場です。',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/801fa6992995fbf628887e770f4c07e0.jpg',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/207/p1-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.46177705603935, 34.62555875027091]
+            },
+            'properties': {
+                'title': '木津川渡船場',
+                'address': '大阪府大阪市住之江区平林北 大正区船町',
+                'date': '木津川渡船場の周辺には、巨大な工場・立ち並ぶ鉄塔・広大な道や空き地を通るトラック以外ほとんど何もない',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/8f1af075ae427dba1a5155b6313f1df4.jpg',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/208/p1-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.45798171771384, 34.632950453586275]
+            },
+            'properties': {
+                'title': '船町渡船場',
+                'address': '大阪府大阪市大正区船町 大正区鶴町',
+                'date': '船町（ふなまち）渡船場は、令和2年度現在、1日平均約163人に利用されています。',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/9813ab52394297b322cda7e3cc4d9bd8.jpg',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/285/p1-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.49237250787706, 34.6129733697545]
+            },
+            'properties': {
+                'title': '住吉大社',
+                'address': '〒559-0007 大阪府大阪市住之江区粉浜西３丁目１',
+                'date': '全国の住吉神社の総本社であり、日本を代表する神社の一つ',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/0609f586991c79c3e1064ae54882c963.png',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/326/p-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.4883216199499, 34.611700718642865]
+            },
+            'properties': {
+                'title': '住吉公園 心字池',
+                'address': '〒559-0002 大阪府大阪市住之江区浜口東１丁目１',
+                'date': '住吉公園の中に、草書体の「心」の形を模した「心字池」という日本庭園があります。',
+                'tags': 'nature',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/374f1a184b69d95ea0fd1a4d7c1b4122.png',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca97/197/p2-r97-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.48969770207242, 34.61317201110916]
+            },
+            'properties': {
+                'title': '住吉公園 花と水の広場',
+                'address': '〒559-0002 大阪府大阪市住之江区浜口東１丁目１',
+                'date': '公園の東に位置する沈庄花壇。季節の花が流れに沿って咲いています。',
+                'tags': 'nature',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/40099f7a4dc68cf6139d75daa5477d5e.png',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/327/p-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.4020868826778, 34.61450991522639]
+            },
+            'properties': {
+                'title': '南港魚つり園護岸',
+                'address': '〒559-0032 大阪府大阪市住之江区南港南６丁目９−３',
+                'date': '南港南の西の果てに、つり園があった。',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/291098cfeb8a5d8f06094c22d29a6792.png',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/1289/p-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.41497842130116, 34.63819701597478]
+            },
+            'properties': {
+                'title': 'コスモタワー',
+                'address': '〒559-0034 大阪府大阪市住之江区南港北１丁目１４−１６',
+                'date': '南港北（咲洲）にある高さ256.0m、地上55階・地下3階建ての超高層ビル',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/14d729cb3296ab446204a551a2db6f86.png',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/1303/p-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.41478295447018, 34.64411832595768]
+            },
+            'properties': {
+                'title': 'シーサイドコスモ',
+                'address': '〒559-0034 大阪府大阪市住之江区南港北１丁目３２',
+                'date': '大阪南港（咲洲）の玄関口、コスモスクエア駅のすぐ北側にある全長1.4kmにも及ぶ海辺',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/efea497fa545e5dcd8bfc288b3fe1d7b.png',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/1304/p-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.42081655887517, 34.633723800118446]
+            },
+            'properties': {
+                'title': '南港ポートタウン',
+                'address': '〒559-0033 大阪府大阪市住之江区南港中４丁目',
+                'date': '大阪南港の人工島「咲洲（さきしま）」の中央部に位置する住宅地、ニュータウン',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/016141812f7ac9d965f4198d7035abf0.png',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/1305/p-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.43565751159537, 34.64200256662909]
+            },
+            'properties': {
+                'title': '咲洲コンテナターミナル',
+                'address': '〒559-0031 大阪府大阪市住之江区南港東９丁目１',
+                'date': '夢洲、咲洲に位置するコンテナ埠頭では、専用コンテナターミナル(C1～4、C8、C9)、メガオペレーターである夢洲コンテナターミナル（株）が一体運営するC10、C11、C12の計9バースが稼働しています。',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/28714c061a24942e652859e0868a09a3.png',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/1306/p-r98-s/'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.50183216544207, 34.61987240976515]
+            },
+            'properties': {
+                'title': '万代池公園',
+                'address': '〒558-0055 大阪府大阪市住吉区万代３丁目７',
+                'date': '大きな池の周りには遊歩道があり、みなさんがウォーキングしたり、ジョギングしたりしています。　公園もあり、ベンチもいっぱいあるのでみんなの憩いの場という雰囲気です。',
+                'tags': 'relax',
+                'img': '',
+                'src': 'https://thumb.fc2.com/480x480/cache.cart-imgs/user_img/pehu/be9348654fd1f30bbc6266c9a56b570f.png',
+                'link': '',
+                'url': 'https://newlifecollection.com/ca98/1311/p-r-s/'
+            }
+        }
+    ]
+};
